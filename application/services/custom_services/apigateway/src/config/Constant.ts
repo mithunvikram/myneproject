f2412abe9c71ed5cd70240a135defafc130daaff
mithunvikram <mithun.vikram@10decoders.in>
2020-08-07 10:01:37 +0000

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://myneproject-app.myneproject.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://myneproject-app.myneproject.svc.cluster.local:3009';
+export const ADMINURL = 'http://myneproject-app.myneproject.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://myneproject-app.myneproject.svc.cluster.local:3008';
